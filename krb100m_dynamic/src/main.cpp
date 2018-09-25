0daad1fe11a81c156aa8ea90b671f4783cecdfdd
zouwubing <1756681646@qq.com>
2018-09-25 23:12:42 +0800

    update config to sever is ok

--- krb100m_dynamic/src/main.cpp
+++ krb100m_dynamic/src/main.cpp
@@ -6,68 +6,65 @@
 ros::Publisher pub;
 ros::Subscriber sub;
 boost::recursive_mutex config_mutex; //I am not sure whether I need it 
-dynamic_reconfigure::Server<krb100m_msg::krb100m_msgConfig> server;
+dynamic_reconfigure::Server<krb100m_msg::krb100m_msgConfig> *parameter_server;
 krb100m_msg::krb100m_msgConfig rt_config;
-void dynamic_reconfigure_update(const krb100m_msg::Dynamic::ConstPtr &msg)  
-{  
-    bool need_update = false;
-    if (msg->kp != rt_config.kp)
-    {
-        rt_config.kp = msg->kp;
-        need_update = true;
-    }
-    if (msg->ki != rt_config.ki)
-    {
-        rt_config.kp = msg->kp;
-        need_update = true;
-    }
-    if (msg->kd != rt_config.kd)
-    {
-        rt_config.kd = msg->kd;
-        need_update = true;
-    }
-    if (msg->left_pwm != rt_config.left_pwm)
-    {
-        rt_config.left_pwm = msg->left_pwm;
-        need_update = true;
-    }
-    if (msg->right_pwm != rt_config.right_pwm)
-    {
-        rt_config.right_pwm = msg->right_pwm;
-        need_update = true;
-    }
-    if (msg->linear_x_vel != rt_config.linear_x_vel)
-    {
-        rt_config.linear_x_vel = msg->linear_x_vel;
-        need_update = true;
-    }
-    if (msg->angular_z_vel != rt_config.angular_z_vel)
-    {
-        rt_config.angular_z_vel = msg->angular_z_vel;
-        need_update = true;
-    }
-    if (msg->dbgpid != rt_config.dbgpid)
-    {
-        rt_config.dbgpid = msg->dbgpid;
-        need_update = true;
-    }
-    if (need_update) 
-    {
-        boost::recursive_mutex::scoped_lock lock(config_mutex); // I am not sure I need it
-        server.updateConfig(rt_config);
-        lock.unlock();
-    }
-}
+
 void dynamic_out_callback(const krb100m_msg::Dynamic::ConstPtr &msg)
 {
-    dynamic_reconfigure_update(msg);
+	bool need_update = false;
+	if (msg->kp != rt_config.kp)
+	{
+		rt_config.kp = msg->kp;
+		need_update = true;
+	}
+	if (msg->ki != rt_config.ki)
+	{
+		rt_config.ki = msg->ki;
+		need_update = true;
+	}
+	if (msg->kd != rt_config.kd)
+	{
+		rt_config.kd = msg->kd;
+		need_update = true;
+	}
+	if (msg->left_pwm != rt_config.left_pwm)
+	{
+		rt_config.left_pwm = msg->left_pwm;
+		need_update = true;
+	}
+	if (msg->right_pwm != rt_config.right_pwm)
+	{
+		rt_config.right_pwm = msg->right_pwm;
+		need_update = true;
+	}
+	if (msg->linear_x_vel != rt_config.linear_x_vel)
+	{
+		rt_config.linear_x_vel = msg->linear_x_vel;
+		need_update = true;
+	}
+	if (msg->angular_z_vel != rt_config.angular_z_vel)
+	{
+		rt_config.angular_z_vel = msg->angular_z_vel;
+		need_update = true;
+	}
+	if (msg->dbgpid != rt_config.dbgpid)
+	{
+		rt_config.dbgpid = msg->dbgpid;
+		need_update = true;
+	}
+	if (need_update) 
+	{
+		boost::recursive_mutex::scoped_lock lock(config_mutex); // I am not sure I need it
+		parameter_server->updateConfig(rt_config);
+		lock.unlock();
+	}
 	//ROS_INFO("Dynamic msg: %d %d %d %d %d %d", 
-			//msg->kp, 
-			//msg->ki, 
-			//msg->kd, 
-			//msg->left_pwm, 
-			//msg->right_pwm, 
-			//msg->dbgpid);
+	//msg->kp, 
+	//msg->ki, 
+	//msg->kd, 
+	//msg->left_pwm, 
+	//msg->right_pwm, 
+	//msg->dbgpid);
 }
 
 void callback(krb100m_msg::krb100m_msgConfig &config, uint32_t level) 
@@ -101,10 +98,12 @@ int main(int argc, char **argv)
 	sub = nh.subscribe("dynamic_out", 100, dynamic_out_callback);
 	pub = nh.advertise<krb100m_msg::Dynamic>("dynamic_in", 1000);
 
+	dynamic_reconfigure::Server<krb100m_msg::krb100m_msgConfig> server;
 	dynamic_reconfigure::Server<krb100m_msg::krb100m_msgConfig>::CallbackType f;
 
 	f = boost::bind(&callback, _1, _2);
 	server.setCallback(f);
+	parameter_server = &server;
 
 	ROS_INFO("Spinning node");
 	ros::spin();
