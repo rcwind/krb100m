48460ddeaf3f0786b813ff88af989b3a3bd8a470
rcwind <251349552@qq.com>
2020-04-05 08:56:30 +0800

    modified udev

--- setup_udev_rule.sh
+++ setup_udev_rule.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 baseDirForScriptSelf=$(cd "$(dirname "$0")"; pwd)
 
-sudo cp $baseDirForScriptSelf/kicc100m-ttyusb.rules /etc/udev/rules.d/
+sudo cp $baseDirForScriptSelf/kicc100a-ttyusb.rules /etc/udev/rules.d/
 #重新插拔设备即可
 sudo udevadm control --reload
